ccf461ff8d53f4d64cca0bd9283a89e7ac0ec8a7
kuba <info@weblead.pl>
2021-08-15 12:42:06 +0200

    first

--- wp-content/themes/consultix-child/inc/footer/footer-style-four.php
+++ wp-content/themes/consultix-child/inc/footer/footer-style-four.php
@@ -1,31 +1,100 @@
 <?php
 /**
- * Header Style One Template
+ * Template for Footer Style Four
  *
  * @package Consultix
  */
-?>
 
-<!-- wraper_header -->
-<header class="wraper_header style-one">
-	<!-- wraper_header_top -->
-	<div class="wraper_header_top">
+?>
+<!-- wraper_footer -->
+<footer class="wraper_footer style-four">
+	<!-- wraper_footer_navigation -->
+	<div class="wraper_footer_navigation">
 		<div class="container">
 			<!-- row -->
-			<div class="row header_top">
-				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-					<!-- header_top_item -->
-					<div class="header_top_item text-left">
-						<ul class="contact">
-							<li class="phone"><a href="tel:<?php echo esc_attr( consultix_global_var( 'top_bar_phone', '', false ) ); ?>"><?php echo esc_html( consultix_global_var( 'top_bar_phone', '', false ) ); ?></a></li>
-							<li class="email"><a href="mailto:<?php echo esc_attr( sanitize_email( consultix_global_var( 'top_bar_email', '', false ) ) ); ?>"><?php echo esc_html( sanitize_email( consultix_global_var( 'top_bar_email', '', false ) ) ); ?></a></li>
-						</ul>
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<!-- footer_navigation -->
+					<div class="footer_navigation">
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'footer',
+								'fallback_cb'    => false,
+							)
+						);
+						?>
 					</div>
-					<!-- header_top_item -->
+					<!-- footer_navigation -->
+				</div>
+			</div>
+			<!-- row -->
+		</div>
+	</div>
+	<!-- wraper_footer_navigation -->
+	<?php
+	$footer_widgets_count = 0;
+	for ( $j = 1; $j <= 4; $j++ ) {
+		if ( is_active_sidebar( 'consultix-footer-area-' . $j ) ) {
+			$footer_widgets_count++;
+		}
+	}
+	if ( $footer_widgets_count > 0 ) {
+	?>
+	<!-- wraper_footer_main -->
+	<div class="wraper_footer_main">
+		<div class="container">
+			<!-- row -->
+			<div class="row footer_main">
+				<?php
+				// Default - Footer 4 column.
+				$footer_class = '';
+				switch ( $footer_widgets_count ) {
+					case 1:
+						$footer_class = '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">';
+						break;
+					case 2:
+						$footer_class = '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">';
+						break;
+					case 3:
+						$footer_class = '<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">';
+						break;
+					default:
+						$footer_class = '<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">';
+				}
+
+				for ( $j = 1; $j <= 4; $j++ ) {
+					if ( is_active_sidebar( 'consultix-footer-area-' . $j ) ) {
+						echo wp_kses_post( $footer_class ) . '<div class="footer_main_item matchHeight">';
+						dynamic_sidebar( 'consultix-footer-area-' . $j );
+						echo '</div>
+						</div>';
+					}
+				}
+				?>
+			</div>
+			<!-- row -->
+		</div>
+	</div>
+	<!-- wraper_footer_main -->
+    <?php
+    }
+    ?>
+	<!-- wraper_footer_copyright -->
+	<div class="wraper_footer_copyright">
+		<div class="container">
+			<!-- row -->
+			<div class="row footer_copyright">
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+					<!-- footer_copyright_item -->
+					<div class="footer_copyright_item text-left">
+					    <p><?php echo wp_kses_post( consultix_global_var( 'footer_copyright_bar', '', false ) ); ?></p>
+					</div>
+					<!-- footer_copyright_item -->
 				</div>
 				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-					<!-- header_top_item -->
-					<div class="header_top_item text-right">
+					<!-- footer_copyright_item -->
+					<div class="footer_copyright_item text-right">
 						<?php
 						if ( true == consultix_global_var( 'social-icon-target', '', false ) ) {
 							$social_target = 'target="_blank"';
@@ -89,116 +158,12 @@
 						</ul>
 						<!-- social -->
 					</div>
-					<!-- header_top_item -->
+					<!-- footer_copyright_item -->
 				</div>
 			</div>
 			<!-- row -->
 		</div>
 	</div>
-	<!-- wraper_header_top -->
-	<!-- wraper_header_main -->
-	<?php if ( true == consultix_global_var( 'header_sticky_choose', '', false ) ) { ?>
-	<div class="wraper_header_main i-am-sticky">
-	<?php } else { ?>
-	<div class="wraper_header_main">
-	<?php } ?>
-		<div class="container">
-			<!-- header_main -->
-			<div class="header_main">
-				<!-- brand-logo -->
-				<div class="brand-logo ">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( consultix_global_var( 'opt-logo-media', 'url', true ) ); ?>" alt="<?php esc_html_e( 'logo', 'consultix' ); ?>"></a>
-				</div>
-				<!-- brand-logo -->
-				<!-- header_main_action -->
-				<div class="header_main_action">
-					<ul>
-						<?php if ( true == consultix_global_var( 'header_search_display', '', false ) ) : ?>
-							<?php if ( 'floating-search' == consultix_global_var( 'header_search_style', '', false ) ) { ?>
-								<li class="floating-searchbar">
-									<i class="fa fa-search"></i>
-									<i class="fa fa-times"></i>
-									<!-- floating-search-bar -->
-									<div class="floating-search-bar">
-										<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-										<div class="form-row">
-											<input type="search" placeholder="<?php echo esc_attr__( 'Search...', 'consultix' ); ?>" value="" name="s" required>
-											<button type="submit"><i class="fa fa-search"></i></button>
-										</div>
-										</form>
-									</div>
-									<!-- floating-search-bar -->
-								</li>
-							<?php } elseif ( 'flyout-search' == consultix_global_var( 'header_search_style', '', false ) ) { ?>
-								<li class="flyout-searchbar-toggle">
-									<i class="fa fa-search"></i>
-									<i class="fa fa-times"></i>
-								</li>
-							<?php } ?>
-						<?php endif; ?>
-						<?php if ( class_exists( 'WooCommerce' ) ) : ?>
-						<li class="header-cart-bar">
-						    <a class="header-cart-bar-icon" href="<?php echo esc_url( wc_get_cart_url() ); ?>">
-							    <i class="fa fa-shopping-cart"></i>
-							    <span class="cart-count"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
-							</a>
-						</li>
-						<?php endif; ?>
-					</ul>
-				</div>
-				<!-- header_main_action -->
-				<!-- responsive-nav -->
-				<div class="responsive-nav hidden-lg hidden-md visible-sm visible-xs" data-responsive-nav-displace="<?php echo esc_attr( consultix_global_var( 'header_mobile_menu_displace', '', false ) ); ?>">
-					<i class="fa fa-bars"></i>
-				</div>
-				<!-- responsive-nav -->
-				<!-- nav -->
-				<nav class="nav visible-lg visible-md hidden-sm hidden-xs">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'top',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-				</nav>
-				<!-- nav -->
-				<div class="clearfix"></div>
-			</div>
-			<!-- header_main -->
-		</div>
-	</div>
-	<!-- wraper_header_main -->
-</header>
-<!-- wraper_header -->
-
-<?php if ( true == consultix_global_var( 'header_search_display', '', false ) ) : ?>
-	<?php if ( 'flyout-search' == consultix_global_var( 'header_search_style', '', false ) ) : ?>
-		<!-- wraper_flyout_search -->
-		<div class="wraper_flyout_search">
-			<div class="table">
-				<div class="table-cell">
-				    <!-- flyout-search-close -->
-				    <div class="flyout-search-close">
-    					<i class="fa fa-times"></i>
-    				</div>
-    				<!-- flyout-search-close -->
-					<!-- flyout_search -->
-					<div class="flyout_search">
-						<!-- search-form -->
-						<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-						<div class="form-row">
-							<input type="search" placeholder="<?php echo esc_attr__( 'Search...', 'consultix' ); ?>" value="" name="s" required>
-							<button type="submit"><i class="fa fa-search"></i></button>
-						</div>
-						</form>
-						<!-- search-form -->
-					</div>
-					<!-- flyout_search -->
-				</div>
-			</div>
-		</div>
-		<!-- wraper_flyout_search -->
-	<?php endif; ?>
-<?php endif; ?>
+	<!-- wraper_footer_copyright -->
+</footer>
+<!-- wraper_footer -->
