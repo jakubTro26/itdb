cba33b256979a6724d34e528d2b845f76be13229
kuba <info@weblead.pl>
2021-09-18 10:18:20 +0200

    first

--- wp-content/plugins/js_composer/include/templates/shortcodes/vc_column.php
+++ wp-content/plugins/js_composer/include/templates/shortcodes/vc_column.php
@@ -92,7 +92,7 @@ $output .= '<div ' . implode( ' ', $wrapper_attributes ) . '>';
 
 $innerColumnClass = 'vc_column-inner ' . esc_attr( trim( vc_shortcode_custom_css_class( $css ) ) );
 if(strpos($innerColumnClass, 'vc_custom_1519806957265') !== false){
-    $innerColumnClass = 'vc_column-inner ' . esc_attr( trim( vc_shortcode_custom_css_class( $css ) ) ) . '"';;
+    $innerColumnClass = 'vc_column-inner ' . esc_attr( trim( vc_shortcode_custom_css_class( $css ) ) ) ;
 } 
 else{
 	$innerColumnClass = 'vc_column-inner ' . esc_attr( trim( vc_shortcode_custom_css_class( $css ) ) );
